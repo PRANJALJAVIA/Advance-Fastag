df904bb1e4cafb0912ff3a7c2f3c85205fdc7a93
sahilsapariya <sahilsapariya03@gmail.com>
2024-02-25 16:20:26 +0530

    created home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import ProjectRoutes from "./Routes";
-
-
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <ProjectRoutes />
-    </div> 
+      <Footer />
+    </div>
   );
 }
 
